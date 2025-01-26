0a1ba8cfda3f9e346a708b10bfb237caa7b4b60b
Caio Sousa <caiosousanl@gmail.com>
2025-01-26 20:50:49 -0300

    chore: add icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,6 +70,10 @@ const TodoList = () => {
     console.log(pomodoroListNew);
   };
 
+  const playPause = (pomodoro: IPomodoro) => () => {
+    console.log(pomodoro, "todo");
+  };
+
   return (
     <div className="min-h-screen bg-gray-600 py-8">
       <div className="max-w-md mx-auto bg-white rounded-lg shadow p-6 text-gray-800">
@@ -88,15 +92,47 @@ const TodoList = () => {
                 key={pomodoro.id}
                 className="mt-2 mb-2 border-emerald-500 border-4"
               >
-                <div className="flex flex-row justify-between">
-                  <div>Pomodoro {index}</div>
-                  <div>...</div>
+                <div className="flex flex-row ">
+                  <div className="basis-2/3">Pomodoro {index}</div>
+                  <div>...</div> {/* todos: change pomodoro duration,  */}
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={playPause(pomodoro)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+                      />
+                    </svg>
+                  </button>
                   <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     onClick={addTask(pomodoro)}
                   >
-                    Add Task
-                  </button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                      />
+                    </svg>
+                  </button>{" "}
                 </div>
                 {pomodoro.tasks &&
                   pomodoro.tasks.map((task) => (
